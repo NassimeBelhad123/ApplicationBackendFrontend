c39e4db389ade1ccea8383f1797832e3cf87a514
Nassime Belhad <bakunass@hotmail.com>
2023-05-21 21:28:18 -0400

    Updates

---
+++ precache-manifest.8fac19849be72c9f32b248f7ded09a1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba89cedc43be3a45adcdb9bae2038b60",
+    "url": "/ApplicationBackendFrontend/index.html"
+  },
+  {
+    "revision": "9cd45bebd92e97b8ae7b",
+    "url": "/ApplicationBackendFrontend/static/css/main.fcdf7237.chunk.css"
+  },
+  {
+    "revision": "929c6ca756e7b689c5ab",
+    "url": "/ApplicationBackendFrontend/static/js/2.ef03b938.chunk.js"
+  },
+  {
+    "revision": "9cd45bebd92e97b8ae7b",
+    "url": "/ApplicationBackendFrontend/static/js/main.9a8da945.chunk.js"
+  },
+  {
+    "revision": "2a4907bc0a0a2703357e",
+    "url": "/ApplicationBackendFrontend/static/js/runtime-main.d0c07552.js"
+  }
+]);
